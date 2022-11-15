2798d8f4a790de4f2fab7781dd06b5e119878dc3
Alex M <alexm0717@hotmail.com>
2022-11-15 08:16:29 -0800

    validations complete

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,7 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Flight, type: :model do
-  describe 'relationships' do
-    it {should belong_to :airline}
+
+RSpec.describe(Flight, type: :model) do
+  describe("relationships") do
+    it { should(belong_to(:airline)) }
+    it { should(have_many(:flight_passengers)) }
+    it { should(have_many(:passengers).through(:flight_passengers)) }
   end
 end
